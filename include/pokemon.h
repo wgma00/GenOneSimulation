e050e8214e75291e350d93e1b34e01e7672fef72
dingram1337 <ingram.1337@gmail>
2016-07-08 22:03:31 -0400

    Added a neutral type and member methods to the pokemon class

--- include/pokemon.h
+++ include/pokemon.h
@@ -7,7 +7,7 @@ class Pokemon {
         Type type1, type2;
         Move move1, move2, move3, move4;
         int HP, Atk, Def, Spc, Spe;
-        int modAtk, modDef, modSpc, modSpe;
+        int mods[4];
 
     public:
         Type getType1();
@@ -17,6 +17,15 @@ class Pokemon {
         int getDef();
         int getSpc();
         int getSpe();
+        int *getMods();
+        void setType1();
+        void setType2();
+        void setHP();
+        void setAtk();
+        void setDef();
+        void setSpc();
+        void setSpe();
+        void addMod()
 };
 
 #endif
