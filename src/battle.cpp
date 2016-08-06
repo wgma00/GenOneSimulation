3ae1b8aa1a72518c5b084f411339b2ffbf20ec51
dingram1337 <ingram.1337@gmail>
2016-08-06 01:54:03 -0400

    Implemented stat modifiers in battle.cpp

--- src/battle.cpp
+++ src/battle.cpp
@@ -6,8 +6,7 @@ double getTypeEffect(Move move, Type type) {
     return typeChart[move.type, type];
 }
 
-int getDamage(Move move, Pokemon poke1,
-        Pokemon poke2, bool crit, double roll) {
+int getDamage(Move move, Pokemon poke1, Pokemon poke2, bool crit, double roll) {
     bool STAB = isSTAB(poke1, move);
     int critboost = crit ? 2 : 1;
 
@@ -19,8 +18,22 @@ double getDamage(Move move, Pokemon poke1, Pokemon poke2, bool STAB) {
     bool phys = isPhysical(move);
     int atk = phys ? poke1.atk : poke1.spc;
     int def = phys ? poke2.def : poke2.spc;
-    double effect = getTypeEffect(move, pokemon.type1())
-            * getTypeEffect(move, pokemon.type2());
+    int atkmod, defmod;
+    double effect = getTypeEffect(move, poke2.type1())
+            * getTypeEffect(move, poke2.type2());
+
+    if (phys) {
+        atkmod = poke1.atkmod() > -1 ? (atkmod + 2) / 2.0 : 2.0 / (2 - atkmod);
+        defmod = poke2.defmod() > -1 ? (defmod + 2) / 2.0 : 2.0 / (2 - defmod);
+    } else {
+        atkmod = poke1.spcmod() > -1 ? (atkmod + 2) / 2.0 : 2.0 / (2 - atkmod);
+        defmod = poke2.spcmod() > -1 ? (defmod + 2) / 2.0 : 2.0 / (2 - defmod);
+    }
+
+    atk *= atkmod;
+    atk = atkmod >= 1 ? std::min(atk, 999) : std::max(atk, 1);
+    def *= defmod;
+    def = defmod >= 1 ? std::min(def, 999) : std::max(def, 1);
 
     // Formula is (((2 * Level + 10)/250) * (Atk/Def) * BP + 2) * Modifiers
     // Modifiers = STAB * Type * Crit * roll
