847846a6d168fcd14b66a0f2e43300468a1c8e8c
William Granados <william.granados@wgma00.me>
2016-08-06 10:15:59 -0400

    fixed up how moves should be stored, added some new moves, made a sample usage case in the test class.

---
+++ include/move.h
@@ -0,0 +1,74 @@
+
+#ifndef GENONESIMULATION_INCLUDE_MOVE
+#define GENONESIMULATION_INCLUDE_MOVE
+
+#include<string>
+#include<cstring>
+#include<sstream>
+#include<cstdlib>
+#include<iostream>
+
+struct Move {
+
+    /**
+     * name, accuracy, base_power, category(attach move, status, special), pp,
+     * self_inflected_status, volatile_status(frz, brn, confusion), 
+     * volatile_status_chance, boosts, healing, type
+     * multiple paramaters are split with | operators.
+     *
+     **/
+
+   
+    private: 
+        std::string name;
+        int accuracy;
+        int base_power;
+        std::string category;
+        int pp;
+        std::string self_inflected_status;
+        std::string volatile_status;
+        std::string volatile_status_chance;
+        std::string boosts;
+        std::string heal;
+        std::string type;
+    public:
+        Move();
+        Move(std::string data);
+        ~Move();
+
+        std::string get_name();
+        void set_name(std::string name);
+
+        int get_accuracy();
+        void set_accuracy(int accuracy);
+
+        int get_base_power();
+        void set_base_power(int base_power);
+        
+        std::string get_category();
+        void set_category(std::string category);
+
+        int get_pp();
+        void set_pp(int pp);
+    
+        std::string get_self_inflected_status();
+        void set_self_inflected_status(std::string self_inflected_status);
+
+        std::string get_volatile_status();
+        void set_volatile_status(std::string volatile_status);
+
+        std::string get_volatile_status_chance();
+        void set_volatile_status_chance(std::string volatile_status_chance);
+
+        std::string get_boosts();
+        void set_boosts(std::string boosts);
+
+        std::string get_heal();
+        void set_heal(std::string heal);
+
+        std::string get_type();
+        void set_type(std::string type);
+
+};
+
+#endif
--- include/pokedata.h
+++ include/pokedata.h
@@ -6,17 +6,6 @@ enum Type = {NORMAL, FIGHTING, FLYING, POISON, GROUND, ROCK, BUG, GHOST, FIRE,
 
 enum Status = {FREEZE, SLEEP, PARALYSIS};
 
-struct Move {
-    int bp;
-    int pp;
-    int acc;
-    Type type;
-    double freezec; // chance of inflicting freeze status
-    double sleepc; // chance of inflicting sleep status
-    double parac; // chance of inflicting paralysis status
-    double confusec; // chance of confusion
-};
-
 double typeChart[16][16] = {
     {1, 1, 1, 1, 1, 0.5, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1},
     {2, 1, 0.5, 0.5, 1, 2, 0.5, 0, 1, 1, 1, 1, 0.5, 2, 0, 1},
