37df00cc7290eb1eb1f3da5b2487f18075637775
dingram1337 <ingram.1337@gmail>
2016-08-06 21:15:42 -0400

    Removed redundant test file and updated state.h

--- include/state.h
+++ include/state.h
@@ -1,26 +1,26 @@
 #ifndef STATE_H
 #define STATE_H
 
-#include "pokemon.h"
+#include "../include/pokemon.h"
 #include <string>
 #include <vector>
 
 class State {
     private:
-        std::vector<Pokemon> team1_(6);
-        std::vector<Pokemon> team2_(6);
+        std::vector<Pokemon> t1;
+        std::vector<Pokemon> t2;
         //BigDecimal prob; placeholder for high precision class
 
     public:
-        State(std::vector<Pokemon*> team1, std::vector<Pokemon*> team2);
+        State(std::vector<Pokemon> team1, std::vector<Pokemon> team2);
         ~State();
 
         std::vector<Pokemon> team1();
         std::vector<Pokemon> team2();
         //BigDecimal probability(); placeholder for high precision class
 
-        void setTeam1(std::vector<Pokemon*> newTeam1);
-        void setTeam2(std::vector<Pokemon*> newTeam2);
+        void setTeam1(std::vector<Pokemon> newTeam1);
+        void setTeam2(std::vector<Pokemon> newTeam2);
         //void setProbability(BigDecimal newDecimal); placeholder for high precision class
 
         std::string ToString();
