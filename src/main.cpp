8fcb0527f5471d03c7a769534a69b738a7276257
dingram1337 <ingram.1337@gmail>
2016-08-20 21:43:20 -0400

    Condensed size of exActions() and started accounting for special cases in the attack case in exActions()

--- src/main.cpp
+++ src/main.cpp
@@ -61,78 +61,67 @@ void exActions(std::vector<Pokemon> &first, std::vector<Pokemon> &second) {
     int critchance = rand() % 512;
     bool crit = critchance < basespe;
     int acc = rand() % 100;
-    int choice;
+    int choice = handleClamp(first); // Clamp checker to prevent switching if clamped
     int damage;
 
-    // Clamp checker to prevent switching if clamped
-    if (first[0].clampturns() == 0) {
-        choice = rand() % first.moves().size() + first.size();
-    } else {
-        choice = rand() % first.moves().size();
-        first[0].setClampTurns(first[0].clampturns() - 1);
-    }
-
     // First poke's moves
-
     if (choice <= first.moves().size() && first[0].canMove()) {
         if (first.moves().size() == 0) { //struggle
             Move m("struggle,100,50,attack,10,TRUE,recoil,100 none,none,normal");
-            damage = getDamage(m, first[0], second[0], crit, ((rand() % 16) + 85)) / 100;
+            damage = getDamage(m, first[0], second[0], crit, (((rand() % 16) + 85)) / 100);
             int recoil = std::floor(damage / 2.0);
             first[0].setCurrentHP(first[0].currentHP() - recoil);
             second[0].setCurrentHP(second[0].currentHP() - damage);
-
-            if (first[0].currentHP() <= 0) { // Faster side KOed
-                first.erase(first.begin());
-                if (first.size() == 0) return;
-                int replacement = rand() % first.size();
-                Pokemon temp = first[0];
-                first[0] = first[replacement];
-                first[replacement] = temp;
-            }
-            if (second[0].currentHP() <=0) { // Slower side KOed
-                second.erase(second.begin());
-                if (second.size() == 0) return;
-                int replacement = rand() % second.size();
-                Pokemon temp = second[0];
-                second[0] = second[replacement];
-                second[replacement] = temp;
-            }
+            handleFaint(first); // Faster side KOed
+            handleFaint(second); // Slower side KOed
         } else { // One of the mon's moves
-                if (accuracy < first[0].moves().at(choice).get_pp()) {
-                    
+            if (accuracy < first[0].moves().at(choice).get_accuracy()) {
+                Move m = first.[0].moves().at(choice);
+
+                // Time for a crapton of conditionals...
+                if (m.get_category() == "attack") {
+                    damage = getDamage(m, first[0], second[0], crit, (((rand() % 16) + 85)) / 100);
+
+                    switch (m.get_name()) {
+                        case "hyperbeam": {
+                            break;
+                        }
+                        case "nightshade": {
+                            break;
+                        }
+                        case "doubleedge": {
+                            break;
+                        }
+                        case "megadrain": {
+                            break;
+                        }
+                        case default: {
+                            break;
+                        }
+                    }
+
+                    second[0].setCurrentHP(second[0].currentHP() - damage);
+                    handleFaint(second); // Slower side KOed
+                    m.set_pp(m.get_pp() - 1);
+                } else if (move.get_category() == "special") {
+
+                } else {
+
+                }
             }
         }
-    } else { // switch
-        choice = choice - first.moves().size();
-
-        if (choice == 0) {
-            choice = (rand() % (first.size() - 1)) + 1;
-        }
-        if (first[0].status() != PARALYSIS && first[0].paramod()) {
-            first[0].setParaMod(false);
-        }
-
-        Pokemon temp = first[0];
-        first[0] = first[choice];
-        first[choice] = temp;
+    } else {
+        // switch
+        handleSwitch(first, choice);
     }
 
     basespe = 0; //placeholder
     critchance = rand() % 512;
     crit = critchance < basespe;
     acc = rand() % 100;
-
-    // Clamp checker to prevent switching if clamped
-    if (second[0].clampturns() == 0) {
-        choice = rand() % second.moves().size() + second.size();
-    } else {
-        choice = rand() % second.moves().size();
-        second[0].setClampTurns(second[0].clampturns() - 1);
-    }
+    choice = handleClamp(second); // Clamp checker to prevent switching if clamped
 
     // Second poke's moves
-
     if (choice <= second.moves().size() && second[0].canMove()) {
         if (second.moves().size() == 0) { //struggle
             Move m("struggle,100,50,attack,10,TRUE,recoil,100 none,none,normal");
@@ -140,42 +129,55 @@ void exActions(std::vector<Pokemon> &first, std::vector<Pokemon> &second) {
             int recoil = std::floor(damage / 2.0);
             second[0].setCurrentHP(first[0].currentHP() - recoil);
             first[0].setCurrentHP(second[0].currentHP() - damage);
-
-            if (first[0].currentHP() <= 0) { // Faster side KOed
-                first.erase(first.begin());
-                if (first.size() == 0) return;
-                int replacement = rand() % first.size();
-                Pokemon temp = first[0];
-                first[0] = first[replacement];
-                first[replacement] = temp;
-            }
-            if (second[0].currentHP() <=0) { // Slower side KOed
-                second.erase(second.begin());
-                if (second.size() == 0) return;
-                int replacement = rand() % second.size();
-                Pokemon temp = second[0];
-                second[0] = second[replacement];
-                second[replacement] = temp;
-            }
+            handleFaint(first); // Faster side KOed
+            handleFaint(second); // Slower side KOed
         } else { // One of the mon's moves
-                if (accuracy < second[0].moves().at(choice).get_pp()) {
+            if (accuracy < second[0].moves().at(choice).get_pp()) {
 
-                }
+            }
         }
-    } else { // switch
-        choice = choice - second.moves().size();
-
-        if (choice == 0) {
-            choice = (rand() % (second.size() - 1)) + 1;
-        }
-        if (second[0].status() != PARALYSIS && second[0].paramod()) {
-            second[0].setParaMod(false);
-        }
-
-        Pokemon temp = second[0];
-        second[0] = second[choice];
-        second[choice] = temp;
+    } else {
+        // switch
+        handleSwitch(second, choice);
     }
+}
+
+void handleFaint(std::vector<Pokemon> &team) {
+    srand(time(NULL));
+
+    if (team[0].currentHP() <=0) { // Slower side KOed
+        team.erase(team.begin());
+        if (team.size() == 0) return;
+        int replacement = rand() % team.size();
+        Pokemon temp = team[0];
+        team[0] = team[replacement];
+        team[replacement] = temp;
+    }
+}
+
+void handleSwitch(std::vector<Pokemon> &team, int choice) {
+    srand(time(NULL));
+    int newchoice = choice - team[0].moves().size();
+
+    if (newchoice == 0) {
+        newchoice = (rand() % (team.size() - 1)) + 1;
+    }
+    if (team[0].status() != PARALYSIS && team[0].paramod()) {
+        team[0].setParaMod(false);
+    }
+}
+
+int handleClamp(std::vector<Pokemon> &team) {
+    int choice;
+
+    if (team[0].clampturns() == 0) {
+        choice = rand() % team[0].moves().size() + team.size();
+    } else {
+        choice = rand() % team[0].moves().size();
+        team[0].setClampTurns(team[0].clampturns() - 1);
+    }
+
+    return choice;
 }
 
 std::vector<Pokemon> getBestTeam(std::vector<std::vector<Pokemon>> teams) {
