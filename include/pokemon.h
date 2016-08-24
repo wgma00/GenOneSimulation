31722ece4e69a3f7cde6cf32d1fb2c052452d37c
William Granados <william.granados@wgma00.me>
2016-08-24 01:53:43 -0400

    made lots of changes to the pokemon class

--- include/pokemon.h
+++ include/pokemon.h
@@ -8,63 +8,68 @@
 
 class Pokemon {
     private:
-        std::vector<Type> types;
-        std::vector<Move> moves;
-        std::vector<int> stats;
-        std::vector<int> mods;
-        enum typeOrder {TYPE1, TYPE2};
-        enum moveOrder {MOVE1, MOVE2, MOVE3, MOVE4};
-        enum regStat {HP, ATK, DEF, SPC, SPE};
-        enum modStat {MODATK, MODDEF, MODSPC, MODSPE};
-        Pkmn name;
-        Status condition;
-        int currentHP;
-        bool isConfused = false;
-        bool isRested = false;
-        bool paraMod = false;
-        int baseSpe;
-        int clampTurns = 0;
-        int sleepTurns = 0;
-        int counterDamage = 0;
+        enum TypeOrder {TYPE1, TYPE2};
+        enum MoveOrder {MOVE1, MOVE2, MOVE3, MOVE4};
+        enum RegStat {HP, ATK, DEF, SPC, SPE};
+        enum ModStat {MODATK, MODDEF, MODSPC, MODSPE};
+
+        std::vector<Type> types_;
+        std::vector<Move*> moves_;
+        std::vector<int> stats_;
+        std::vector<int> mods_;
+
+        Pkmn name_;
+        Status condition_;
+        int currentHP_;
+        bool confused_;
+        bool rested_;
+        bool paraMod_;
+        int baseSpe_;
+        int clampTurns_;
+        int sleepTurns_;
+        int counterDamage_;
 
     public:
-        Pokemon(Pkmn name, std::vector<Type> types, std::vector<Move> moves,
-                std::vector<int> stats, std::vector<int> mods, Status status);
+        Pokemon();
+        Pokemon(Pkmn name, std::vector<Type> types, std::vector<Move*> moves,
+                std::vector<int> stats, std::vector<int> mods, Status status,
+                int baseSpe);
+        Pokemon(std::string data, std::vector<Move*>moves);
         ~Pokemon();
 
         Type type1();
         Type type2();
-        Move move1();
-        Move move2();
-        Move move3();
-        Move move4();
-        std::vector<Move> moves();
-        int hp();
-        int currentHP();
-        int atk();
-        int def();
-        int spc();
-        int spe();
-        int atkmod();
-        int defmod();
-        int spcmod();
-        int spemod();
-        Pkmn name();
-        Status status();
-        bool confused();
-        bool rested();
-        bool paramod();
-        int basespe();
-        int clampturns();
-        int sleepturns();
-        int counterdamage();
+        Move* move1();
+        Move* move2();
+        Move* move3();
+        Move* move4();
+        std::vector<Move*> getMoves();
+        int getHP();
+        int getCurrentHP();
+        int getAtk();
+        int getDef();
+        int getSpc();
+        int getSpe();
+        int getAtkmod();
+        int getDefmod();
+        int getSpcmod();
+        int getSpemod();
+        Pkmn getName();
+        Status getCondition();
+        bool isConfused();
+        bool isRested();
+        bool getParamod();
+        int getBasespe();
+        int getClampturns();
+        int getSleepturns();
+        int getCounterDamage();
 
         void setType1(Type newType);
         void setType2(Type newType);
-        void setMove1(Move newMove);
-        void setMove2(Move newMove);
-        void setMove3(Move newMove);
-        void setMove4(Move newMove);
+        void setMove1(Move* newMove);
+        void setMove2(Move* newMove);
+        void setMove3(Move* newMove);
+        void setMove4(Move* newMove);
         void setHP(int newHP);
         void setCurrentHP(int newCurrentHP);
         void setAtk(int newAtk);
--- src/pokemon.cpp
+++ src/pokemon.cpp
@@ -2,18 +2,86 @@
 #include <vector>
 #include <algorithm>
 
-Pokemon::Pokemon(Pkmn name, std::vector<Type> types, std::vector<Move> moves,
-        std::vector<int> stats, std::vector<int> mods, Status status, int baseSpe) {
-    this->condition = status;
-    this->name = name;
-    this->currentHP = this->stats.at(0);
-    this->baseSpe = baseSpe;
-    for (int i = 0; i < 5; i++) {
-        this->types.at(std::min(i, 1)) = types.at(std::min(i, 2));
-        this->moves.at(std::min(i, 3)) = moves.at(std::min(i, 3));
-        this->mods.at(std::min(i, 3)) = mods.at(std::min(i, 3));
-        this->stats.at(std::min(i, 4)) = stats.at(std::min(i, 4));
+Pokemon::Pokemon(Pkmn name, std::vector<Type> types, std::vector<Move*> moves,
+        std::vector<int> stats, std::vector<int> mods, Status status,
+        int baseSpe) {
+    this->types_.resize(2);
+    this->moves_.resize(4);
+    this->setName(name);
+    // accounting for multiple types
+    if((int)types.size() == 2){
+        this->setType1(types[TYPE1]);
+        this->setType2(types[TYPE2]);
     }
+    else{
+        this->setType1(types[TYPE1]);
+    }
+    // setting up moves, no need to error check since it should always be
+    // four moves, right?
+    this->setMove1(moves[MOVE1]);
+    this->setMove2(moves[MOVE2]);
+    this->setMove3(moves[MOVE3]);
+    this->setMove4(moves[MOVE4]);
+    // same deal wit these
+    this->setHP(stats[HP]);
+    this->setCurrentHP(stats[HP]);
+    this->setAtk(stats[ATK]);
+    this->setDef(stats[DEF]);
+    this->setSpc(stats[SPC]);
+    this->setSpe(stats[SPE]);
+    // same deal with these
+    this->setAtkMod(mods[MODATK]);
+    this->setDefMod(mods[MODDEF]);
+    this->setSpcMod(mods[MODSPC]);
+    this->setSpeMod(mods[MODSPE]);
+    this->setStatus(status);
+    this->setBaseSpe(baseSpe);
+    // these are just misceallanous intializations
+    this->setConfusion(false);
+    this->setRested(false);
+    this->setParaMod(false);
+    this->setClampTurns(0);
+    this->setSleepTurns(0);
+    this->setCounterDamage(0);
+}
+
+/**
+ * Here we will parse a string in the following format: 
+ * "Name,Type(s),HP,Attack,Defense,Special,Speed,Basespeed"
+ **/
+Pokemon::Pokemon(std::string data, std::vector<Move*>moves){
+    std::stringstream ss(data);
+    std::string field;
+    Pkmn name = NULL_PKMN;
+    std::vector<Type>types;
+    std::vector<int>stats;
+    std::vector<int>mods = {0,0,0,0};
+    Status status = NONE;
+    int baseSpe = 0;
+    // here we will parse the 7 fields, taking into account fields with 
+    // multiple paramaters like Type 
+    for(int i = 0;i < 8;i++){
+        getline(ss, field, ',');
+        if(i == 0)
+            name = to_pokemon(field);
+        if(i == 1){
+           if(field.find('|') == std::string::npos)
+              types.push_back(to_type(field)); 
+           else{
+              int idx = field.find('|');
+              std::cout << field.substr(0,idx) << std::endl;
+              std::cout << field.substr(idx+1,field.size()-idx) << std::endl;
+              types.push_back(to_type(field.substr(0,idx))); 
+              types.push_back(to_type(field.substr(idx+1, field.size()-idx)));
+           }
+        }
+        // HP, ATK, DEF, SPC, SPE
+        if(i >= 2 && i <= 6)
+            stats.push_back(atoi(field.c_str()));
+        if(i ==  7)
+            baseSpe = atoi(field.c_str()); 
+    }
+    Pokemon(name, types, moves, stats, mods, status, baseSpe);
 }
 
 Pokemon::~Pokemon() {
@@ -21,205 +89,205 @@ Pokemon::~Pokemon() {
 }
 
 Type Pokemon::type1() {
-    return this->types.at(TYPE1);
+    return this->types_[TYPE1];
 }
 
 Type Pokemon::type2() {
-    return this->types.at(TYPE2);
+    return this->types_[TYPE2];
 }
 
-Move Pokemon::move1() {
-    return this->moves.at(MOVE1);
+Move* Pokemon::move1() {
+    return this->moves_[MOVE1];
 }
 
-Move Pokemon::move2() {
-    return this->moves.at(MOVE2);
+Move* Pokemon::move2() {
+    return this->moves_[MOVE2];
 }
 
-Move Pokemon::move3() {
-    return this->moves.at(MOVE3);
+Move* Pokemon::move3() {
+    return this->moves_[MOVE3];
 }
 
-Move Pokemon::move4() {
-    return this->moves.at(MOVE4);
+Move* Pokemon::move4() {
+    return this->moves_[MOVE4];
 }
 
-std::vector<Move> Pokemon::moves() {
-    return this->moves;
+std::vector<Move*> Pokemon::getMoves() {
+    return this->moves_;
 }
 
-int Pokemon::hp() {
-    return this->stats.at(HP);
+int Pokemon::getHP() {
+    return this->stats_[HP];
 }
 
-int Pokemon::currentHP() {
-    return this->currentHP;
+int Pokemon::getCurrentHP() {
+    return this->currentHP_;
 }
 
-int Pokemon::atk() {
-    return this->stats.at(ATK);
+int Pokemon::getAtk() {
+    return this->stats_[ATK];
 }
 
-int Pokemon::def() {
-    return this->stats.at(DEF);
+int Pokemon::getDef() {
+    return this->stats_[DEF];
 }
 
-int Pokemon::spc() {
-    return this->stats.at(SPC);
+int Pokemon::getSpc() {
+    return this->stats_[SPC];
 }
 
-int Pokemon::spe() {
-    return this->stats.at(SPE);
+int Pokemon::getSpe() {
+    return this->stats_[SPE];
 }
 
-int Pokemon::atkmod() {
-    return this->mods.at(ATK);
+int Pokemon::getAtkmod() {
+    return this->mods_[ATK];
 }
 
-int Pokemon::defmod() {
-    return this->mods.at(DEF);
+int Pokemon::getDefmod() {
+    return this->mods_[DEF];
 }
 
-int Pokemon::spcmod() {
-    return this->mods.at(SPC);
+int Pokemon::getSpcmod() {
+    return this->mods_[SPC];
 }
 
-int Pokemon::spemod() {
-    return this->mods.at(SPE);
+int Pokemon::getSpemod() {
+    return this->mods_[SPE];
 }
 
-Pkmn Pokemon::name(){
-    return this->name;
+Pkmn Pokemon::getName(){
+    return this->name_;
 }
 
-Status Pokemon::status() {
-    return this->condition;
+Status Pokemon::getCondition() {
+    return this->condition_;
 }
 
-bool Pokemon::confused() {
-    return this->isConfused;
+bool Pokemon::isConfused() {
+    return this->confused_;
 }
 
-bool Pokemon::rested() {
-    return this->isRested;
+bool Pokemon::isRested() {
+    return this->rested_;
 }
 
-bool Pokemon::paramod() {
-    return this->paraMod;
+bool Pokemon::getParamod() {
+    return this->paraMod_;
 }
 
-int Pokemon::basespe() {
-    return this->baseSpe;
+int Pokemon::getBasespe() {
+    return this->baseSpe_;
 }
 
-int Pokemon::clampturns() {
-    return this->clampTurns;
+int Pokemon::getClampturns() {
+    return this->clampTurns_;
 }
 
-int Pokemon::sleepturns() {
-    return this->sleepTurns;
+int Pokemon::getSleepturns() {
+    return this->sleepTurns_;
 }
 
-int Pokemon::counterDamage() {
-    return this->counterDamage;
+int Pokemon::getCounterDamage() {
+    return this->counterDamage_;
 }
 
 void Pokemon::setType1(Type newType) {
-    this->types.at(TYPE1) = newType;
+    this->types_[TYPE1] = newType;
 }
 
 void Pokemon::setType2(Type newType) {
-    this->types.at(TYPE2) = newType;
+    this->types_[TYPE2] = newType;
 }
 
-void Pokemon::setMove1(Move newMove) {
-    this->moves.at(MOVE1) = newMove;
+void Pokemon::setMove1(Move* newMove) {
+    this->moves_[MOVE1] = newMove;
 }
 
-void Pokemon::setMove2(Move newMove) {
-    this->moves.at(MOVE2) = newMove;
+void Pokemon::setMove2(Move* newMove) {
+    this->moves_[MOVE2] = newMove;
 }
 
-void Pokemon::setMove3(Move newMove) {
-    this->moves.at(MOVE3) = newMove;
+void Pokemon::setMove3(Move* newMove) {
+    this->moves_[MOVE3] = newMove;
 }
 
-void Pokemon::setMove4(Move newMove) {
-    this->moves.at(MOVE4) = newMove;
+void Pokemon::setMove4(Move* newMove) {
+    this->moves_[MOVE4] = newMove;
 }
 
 void Pokemon::setHP(int newHP) {
-    this->stats.at(HP) = newHP;
+    this->stats_[HP] = newHP;
 }
 
 void Pokemon::setCurrentHP(int newCurrentHP) {
-    this->currentHP = newCurrentHP;
+    this->currentHP_ = newCurrentHP;
 }
 
 void Pokemon::setAtk(int newAtk) {
-    this->stats.at(ATK) = newAtk;
+    this->stats_[ATK] = newAtk;
 }
 
 void Pokemon::setDef(int newDef) {
-    this->stats.at(DEF) = newDef;
+    this->stats_[DEF] = newDef;
 }
 
 void Pokemon::setSpc(int newSpc) {
-    this->stats.at(SPC) = newSpc;
+    this->stats_[SPC] = newSpc;
 }
 
 void Pokemon::setSpe(int newSpe) {
-    this->stats.at(SPE) = newSpe;
+    this->stats_[SPE] = newSpe;
 }
 
 void Pokemon::setAtkMod(int newModAtk) {
-    this->mods.at(MODATK) = newModAtk;
+    this->mods_[MODATK] = newModAtk;
 }
 
 void Pokemon::setDefMod(int newModDef) {
-    this->mods.at(MODDEF) = newModDef;
+    this->mods_[MODDEF] = newModDef;
 }
 
 void Pokemon::setSpcMod(int newModSpc) {
-    this->mods.at(MODSPC) = newModSpc;
+    this->mods_[MODSPC] = newModSpc;
 }
-
 void Pokemon::setSpeMod(int newModSpe) {
-    this->mods.at(MODSPE) = newModSpe;
+    this->mods_[MODSPE] = newModSpe;
 }
 
 void Pokemon::setName(Pkmn name){
-    this->name = name;
+    this->name_ = name;
 }
 
 void Pokemon::setStatus(Status newStatus) {
-    this->condition = newStatus;
+    this->condition_ = newStatus;
 }
 
 void Pokemon::setConfusion(bool newConfusion) {
-    this->isConfused = confusion;
+    this->confused_= newConfusion;
 }
 
 void Pokemon::setRested(bool newRested) {
-    this->isRested = newRested;
+    this->rested_ = newRested;
 }
 
 void Pokemon::setParaMod(bool newParaMod) {
-    this->paraMod = newParaMod;
+    this->paraMod_ = newParaMod;
 }
 
 void Pokemon::setClampTurns(int newClampTurns) {
-    this->clampTurns = newClampTurns;
+    this->clampTurns_ = newClampTurns;
 }
 
 void Pokemon::setSleepTurns(int newSleepTurns) {
-    this->sleepTurns = newSleepTurns;
+    this->sleepTurns_ = newSleepTurns;
 }
 
 void Pokemon::setCounterDamage(int newCounterDamage) {
-    this->counterDamage = newCounterDamage;
+    this->counterDamage_ = newCounterDamage;
 }
 
 void Pokemon::setBaseSpe(int newBaseSpe) {
-    this->baseSpe = newBaseSpe;
+    this->baseSpe_ = newBaseSpe;
 }
+
